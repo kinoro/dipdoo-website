62bebb29b8017edd3e7592f12013c210fe6deb18
Russ King <kinorouk@gmail.com>
2022-06-06 14:28:56 +0100

    various

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  authUrl: "https://prod.dipdoo.net/api/auth",
-  baseUrl: "https://prod.dipdoo.net/api",
+  authUrl: "https://prod-server.dipdoo.net:18200/api/auth",
+  baseUrl: "https://prod-server.dipdoo.net:18202/api",
   awsUrl: "https://s3.eu-west-2.amazonaws.com/dipdoo-images",
-  useTestData: false
+  useTestData: false,
+  recaptchaSiteKey: '6LdwpjcgAAAAAIkfbzgCfiKrThGZhqiUGT_WHRGG',
 };
